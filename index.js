06464e2120ff951a236c1acca9bc8ebfabfc4c67
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-11-25 23:52:18 +0600

    products api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 
@@ -27,6 +27,13 @@ async function run() {
         .project({ category: 1, picture: 1, adds: 1 })
         .toArray();
       res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
     });
   } finally {
   }
